b0e9626fd23244f3e951a4591550ece42c7c47e2
Aixa Filsinger <macafil99@gmail.com>
2023-05-25 21:36:20 -0300

    se agrego estilo al componente titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import Titulo from './Components/Titulo';
+import { Container } from 'react-bootstrap';
 
 function App() {
   
 
   return (
     <>
+     <Container className='main my-5'>
+      <Titulo></Titulo>
       
+    </Container>
+    <footer className='bg-secondary p-3 text-center fs-4 text-light'>
+        ©Todos los derechos reservados™
+      </footer>
+  
     </>
-  )
+   )
 }
 
 export default App
---
+++ src/Components/Titulo.jsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+const Titulo = () => {
+    return (
+        <Container className="titulo">
+            <h1 className="text-center display-1 fw-semibold fst-italic mb-5">Formulario</h1>
+        </Container>
+    );
+};
+
+export default Titulo;
